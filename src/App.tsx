f86c090edde787a3a37bf1d7c4d1ffdfc8892975
Emmy Manning <emswoo@EM.local>
2026-03-15 16:48:01 -0400

    feature: create mobile first grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import PredictionInput from "./sections/PredictionInput";
+
 const App = () => {
   return (
-    <div>
+    <div className="grid grid-cols-1 gap-6 bg-gray-100 min-h-screen lg:grid-cols-3 lg:h-screen">
+      <div className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1">
+        <PredictionInput />
+      </div>
+      <div className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1"></div>
+      <div className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1"></div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
