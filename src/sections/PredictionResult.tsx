04047cb6d69a3a26099efb4ac2b82c9ed8e0f3fb
Emmy Manning <emswoo@EM.local>
2026-03-16 10:40:12 -0400

    feature: add scroll

--- src/sections/PredictionResult.tsx
+++ src/sections/PredictionResult.tsx
@@ -4,8 +4,20 @@ type PredictionResultProps = {
 
 const PredictionResult = ({ title }: PredictionResultProps) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col lg:h-80">
       <h1 className="text-gray-600 font-bold">{title}</h1>
+      <div className="lg:overflow-y-auto p-2">
+        <p className="lg:overflow-scroll">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque suscipit eligendi beatae possimus amet laudantium officia ex, esse quasi iure aliquam sequi veniam iusto veritatis voluptas
+          voluptate explicabo, voluptatum accusantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque suscipit eligendi beatae possimus amet laudantium officia ex, esse quasi iure
+          aliquam sequi veniam iusto veritatis voluptas voluptate explicabo, voluptatum accusantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque suscipit eligendi beatae possimus
+          amet laudantium officia ex, esse quasi iure aliquam sequi veniam iusto veritatis voluptas voluptate explicabo, voluptatum accusantium. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Itaque suscipit eligendi beatae possimus amet laudantium officia ex, esse quasi iure aliquam sequi veniam iusto veritatis voluptas voluptate explicabo, voluptatum accusantium. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Itaque suscipit eligendi beatae possimus amet laudantium officia ex, esse quasi iure aliquam sequi veniam iusto veritatis voluptas
+          voluptate explicabo, voluptatum accusantium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque suscipit eligendi beatae possimus amet laudantium officia ex, esse quasi iure
+          aliquam sequi veniam iusto veritatis voluptas voluptate explicabo, voluptatum accusantium.
+        </p>
+      </div>
     </div>
   );
 };
