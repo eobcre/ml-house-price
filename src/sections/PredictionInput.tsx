9841fc02d9cbec749bbe829471d78ba7fab1eab1
Emmy Manning <emswoo@EM.local>
2026-03-16 09:48:18 -0400

    new: create input

--- src/sections/PredictionInput.tsx
+++ src/sections/PredictionInput.tsx
@@ -1,7 +1,16 @@
+import Input from "../components/Input ";
+import Button from "../components/Button";
+
 const PredictionInput = () => {
   return (
-    <div>
-      <div></div>
+    <div className="flex flex-col gap-7">
+      <h1 className="text-gray-600 font-bold">Prediction Search</h1>
+      <Input label="Bedrooms" />
+      <Input label="Bathrooms" />
+      <Input label="Square Feet" />
+      <Input label="Year Built" />
+      <Input label="Zip Code" />
+      <Button name="Search" />
     </div>
   );
 };
