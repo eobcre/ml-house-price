339e90c11d6069a5d4fea719d16a415fe46316a8
Emmy Manning <emswoo@EM.local>
2026-03-16 09:49:06 -0400

    refactor: fixed layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import PredictionInput from "./sections/PredictionInput";
 
 const App = () => {
   return (
-    <div className="grid grid-cols-1 gap-6 bg-gray-100 min-h-screen lg:grid-cols-3 lg:h-screen">
-      <div className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1">
+    <div className="grid grid-cols-1 gap-6 bg-gray-100 p-6 min-h-screen lg:grid-cols-3 lg:p-14 lg:h-screen">
+      <section className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1">
         <PredictionInput />
+      </section>
+      <div className="grid grid-rows-2 gap-4">
+        <section className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1"></section>
+        <section className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg: col-span-1"></section>
       </div>
-      <div className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1"></div>
       <div className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1"></div>
     </div>
   );
