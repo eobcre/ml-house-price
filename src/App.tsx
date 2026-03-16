b309221a9766e17a69d13ab3d6d049f7825eb37e
Emmy Manning <emswoo@EM.local>
2026-03-16 11:19:34 -0400

    style: fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,18 @@ import PipelineWorkflow from "./sections/PipelineWorkflow";
 const App = () => {
   return (
     <div className="grid grid-cols-1 gap-6 bg-gray-100 p-6 min-h-screen lg:grid-cols-3 lg:p-14 lg:h-screen">
-      <section className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1">
+      <section className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 h-full lg:col-span-1">
         <PredictionInput title="Prediction Search" />
       </section>
-      <div className="grid grid-rows-2 gap-4">
+      <div className="grid grid-rows-2 gap-4 h-full">
         <section className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1">
           <PredictionResult title="Prediction Result" />
         </section>
-        <section className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg: col-span-1">
+        <section className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1">
           <ModelMetrics title="Model Metrics" />
         </section>
       </div>
-      <section className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1">
+      <section className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 h-full lg:col-span-1">
         <PipelineWorkflow title="Pipeline Workflow" />
       </section>
     </div>
