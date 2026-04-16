7afef7bb14634a49f5221da5ffd805ea26e02aca
Emmy Manning <emswoo@EM.local>
2026-04-16 11:12:41 -0400

    test: send request with dummy payload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,39 @@ import ModelMetrics from "./sections/ModelMetrics";
 import PipelineWorkflow from "./sections/PipelineWorkflow";
 
 const App = () => {
+  // test with dummy payload
+  const handleSearch = async () => {
+    try {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/predict`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          bedrooms: 4,
+          bathrooms: 2,
+          sqft_living: 2000,
+          yr_built: 2000,
+          zipcode: 98052,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.error || "Error.");
+      }
+
+      console.log("SUCCESS!!!", data);
+    } catch (err: any) {
+      console.error("ERROR...", err.message);
+    }
+  };
+
   return (
     <div className="grid grid-cols-1 gap-6 bg-gray-100 p-6 min-h-screen lg:grid-cols-3 lg:p-14 lg:h-screen">
       <section className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 h-full lg:col-span-1">
-        <PredictionInput title="Prediction Search" />
+        <PredictionInput title="Prediction Search" handleSearch={handleSearch} />
       </section>
       <div className="grid grid-rows-2 gap-4 h-full">
         <section className="bg-white border border-gray-200 rounded-2xl shadow-sm p-6 lg:col-span-1">
