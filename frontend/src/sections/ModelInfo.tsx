4b40b0717d87c7045c9dbfe2f71582d24ad2f94e
Emmy Manning <emswoo@EM.local>
2026-04-21 14:13:32 -0400

    feature: add content

--- frontend/src/sections/ModelInfo.tsx
+++ frontend/src/sections/ModelInfo.tsx
@@ -14,10 +14,27 @@ type ModelInfoProps = {
 };
 
 const ModelInfo = ({ title, result }: ModelInfoProps) => {
+  const items = [
+    { id: 1, title: "Algorithm", desc: `Random Forest Regressor` },
+    { id: 2, title: "Features", desc: `bedrooms, bathrooms, sqft_living, yr_built, zipcode` },
+    { id: 3, title: "Dataset", desc: `House Sales in King County, USA` },
+  ];
+
   return (
     <div>
       <h1 className="text-gray-600 font-bold">{title}</h1>
-      {result !== null ? <div></div> : <p className="col-span-2 text-gray-400 italic mt-2">Enter inputs to see the model info.</p>}
+      {result !== null ? (
+        <>
+          {items.map((item) => (
+            <div className="col-span-2 text-gray-600 text-sm mt-4">
+              <h2 className="font-semibold mb-2">{item.title}</h2>
+              <p>{item.desc}</p>
+            </div>
+          ))}
+        </>
+      ) : (
+        <p className="col-span-2 text-gray-400 italic mt-2">Enter inputs to see the model info.</p>
+      )}
     </div>
   );
 };
