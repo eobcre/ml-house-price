bf911577c599ce310609c884fbc3d548c999f05d
Emmy Manning <emswoo@EM.local>
2026-04-28 15:38:16 -0400

    fix: for deploy

--- frontend/src/sections/PredictionInsights.tsx
+++ frontend/src/sections/PredictionInsights.tsx
@@ -13,11 +13,28 @@ type InsightProps = {
   result: ResultData | null;
 };
 
-const PredictionInsights = ({ title, result }: InsightProps) => {
+const PredictionInsights = ({ title }: InsightProps) => {
+  const items = [
+    { id: 1, title: "Mid-size home", desc: `Lorem Lorem Lorem` },
+    { id: 2, title: "Established home", desc: `Lorem Lorem Lorem` },
+    { id: 3, title: "Estimated range", desc: `Lorem Lorem Lorem` },
+  ];
+
   return (
-    <div>
-      <h1 className="text-gray-600 font-bold">{title}</h1>
-      {result !== null ? <div></div> : <p className="col-span-2 text-gray-400 italic mt-2">Enter inputs to see the prediction insights.</p>}
+    <div className="">
+      <h1 className="text-gray-600 font-bold mb-3">{title}</h1>
+      {/* {result !== null ? ( */}
+      <>
+        {items.map((item) => (
+          <div className="col-span-2 mt-4 text-sm text-gray-600">
+            <h2 className="font-semibold mb-2">{item.title}</h2>
+            <p>{item.desc}</p>
+          </div>
+        ))}
+      </>
+      {/* ) : (
+        <p className="col-span-2 text-gray-400 italic mt-2">Enter inputs to see the prediction insights.</p>
+      )} */}
     </div>
   );
 };
