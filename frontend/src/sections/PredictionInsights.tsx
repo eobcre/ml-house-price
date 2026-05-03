7afb4c9d823d66e98bfc14ec83697716ef8fbe40
Emmy Manning <emswoo@EM.local>
2026-05-02 21:10:42 -0400

    feature: add estimated range

--- frontend/src/sections/PredictionInsights.tsx
+++ frontend/src/sections/PredictionInsights.tsx
@@ -1,4 +1,4 @@
-// result state props
+// result props
 type ResultData = {
   prediction: number;
   metrics: {
@@ -11,30 +11,43 @@ type ResultData = {
 type InsightProps = {
   title: string;
   result: ResultData | null;
+  formatCurrency: (value: number) => string;
 };
 
-const PredictionInsights = ({ title }: InsightProps) => {
+const PredictionInsights = ({ title, result, formatCurrency }: InsightProps) => {
+  //console.log("result:", result);
+
+  if (!result) {
+    return (
+      <div>
+        <h1 className="text-gray-600 font-bold mb-3">{title}</h1>
+        <p className="col-span-2 text-gray-400 italic mt-2">Enter inputs to see the insights.</p>
+      </div>
+    );
+  }
+
+  // estimated range
+  const low = Math.max(0, result.prediction - result.metrics.mae);
+  const high = result.prediction + result.metrics.mae;
+
+  const estimatedRangeText = `${formatCurrency(low)} - ${formatCurrency(high)}`;
+
+  // data
   const items = [
-    { id: 1, title: "Mid-size home", desc: `Lorem Lorem Lorem` },
-    { id: 2, title: "Established home", desc: `Lorem Lorem Lorem` },
-    { id: 3, title: "Estimated range", desc: `Lorem Lorem Lorem` },
+    { id: 1, title: "Estimated range", desc: estimatedRangeText },
+    { id: 2, title: "Mid-size home", desc: `Lorem Lorem Lorem` },
+    { id: 3, title: "Established home", desc: `Lorem Lorem Lorem` },
   ];
 
   return (
-    <div className="">
+    <div>
       <h1 className="text-gray-600 font-bold mb-3">{title}</h1>
-      {/* {result !== null ? ( */}
-      <>
-        {items.map((item) => (
-          <div className="col-span-2 mt-4 text-sm text-gray-600">
-            <h2 className="font-semibold mb-2">{item.title}</h2>
-            <p>{item.desc}</p>
-          </div>
-        ))}
-      </>
-      {/* ) : (
-        <p className="col-span-2 text-gray-400 italic mt-2">Enter inputs to see the prediction insights.</p>
-      )} */}
+      {items.map((item) => (
+        <div key={item.id} className="col-span-2 mt-4 text-sm text-gray-600">
+          <h2 className="font-semibold mb-2">{item.title}</h2>
+          <p>{item.desc}</p>
+        </div>
+      ))}
     </div>
   );
 };
